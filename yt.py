28deae2565e4e1274b479c14cd4e9c74d12f827d
Crownss <58449385+Crownss@users.noreply.github.com>
2020-11-23 17:04:51 +0700

    youtube downloader python
    for youtube downlaod anything use python

---
+++ yt.py
@@ -0,0 +1,70 @@
+from pytube import YouTube
+import youtube_dl
+import subprocess
+import time
+
+video_url = input("link: ")
+
+def mp4():
+    global video_url
+    yt = YouTube(video_url)
+    print(yt.title)
+    print(yt.description)
+    stream = yt.streams.first()
+    print('\n')
+    print("downloading.....")
+    stream.download('/home/sir')
+    print('Succes')
+    print('\n')
+
+def mp3():
+    # Ask the user for the video they want to download
+    global video_url
+    # Download and convert to mp3 and store in downloads folder
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+    print("Succes")
+    print('\n')
+
+class menu:
+    def a():
+        b = input("format[mp3/mp4]: ")
+        if b == 'mp3':
+            mp3()
+            time.sleep(2)
+        elif b == 'mp4':
+            mp4()
+            time.sleep(2)
+        else:
+            print("PILIH YG BENER")
+            time.sleep(3)
+            exit()
+    def c():
+        d = input("want to input another?: [y/n]")
+        if d == 'y':
+            video_url = input("link: ")
+            menu.a()
+            time.sleep(2)
+            menu.c()
+        elif d == 'n':
+            exit()
+        else:
+            exit()
+
+while True:
+    menu.a()
+    menu.c()
